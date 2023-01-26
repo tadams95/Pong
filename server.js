4661bfc86fcc92bf03fca43a764a7217a80d4269
tadams95 <tyrelleadams28@gmail.com>
2023-01-25 18:18:45 -0800

    Load and start game with two players connect

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const server = require("http").createServer();
 
-const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST']
-    }
-  });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 const PORT = 3000;
 
@@ -13,6 +13,18 @@ server.listen(PORT);
 
 console.log(`Listening on Port ${PORT}...`);
 
+let readyPlayerCount = 0;
+
 io.on("connection", (socket) => {
-    console.log("a user connected...", socket.id)
-})
+  console.log("a user connected...", socket.id);
+
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit("startGame", socket.id)
+    }
+  });
+});
