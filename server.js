0c1afe8f421199e9a5182328a93476caae7c3748
tadams95 <tyrelleadams28@gmail.com>
2023-01-25 17:54:46 -0800

    First Commit - initialize server

---
+++ server.js
@@ -0,0 +1,18 @@
+const server = require("http").createServer();
+
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+const PORT = 3000;
+
+server.listen(PORT);
+
+console.log(`Listening on Port ${PORT}...`);
+
+io.on("connection", (socket) => {
+    console.log("a user connected...", socket.id)
+})
